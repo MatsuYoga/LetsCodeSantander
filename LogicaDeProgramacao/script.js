4b21805db133dd97bf1cb30e6a3f2fdc7eef28e3
Jorge Soares <jorgejunioalvessoares@hotmail.com>
2021-07-03 15:33:10 -0300

    Condicionais JS

--- LogicaDeProgramacao/script.js
+++ LogicaDeProgramacao/script.js
@@ -1,4 +1,35 @@
-var a = 10;
-var b = 13.5;
+let numero = -5;
 
-console.log(b);
+if (numero > 0){
+    console.log('Numero positivo');
+} else if (numero == 0){
+    console.log('numero zero');
+}
+else {
+    console.log('Numero Negativo');
+}
+
+//tenário
+
+let numero1 = 1;
+let paridade = numero1 % 2 == 0 ? 'par' : 'impar';
+
+console.log(paridade);
+
+//switch
+
+let sinal = 'veramelho';
+
+switch(sinal){
+    case 'verde' : 
+        console.log('passa');
+        break;
+    case 'amarelo' :
+        console.log('cuidado');
+        break;
+    case 'vermelho' :
+        console.log('fechado, não passe');
+        break;
+    
+    default : console.log('sinal inválido');
+}
